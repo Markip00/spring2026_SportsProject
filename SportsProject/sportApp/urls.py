389b63cf7d340068ab11872cd758fcc53f5d2037
Jchi <JuanNino05@outlook.com>
2026-04-06 01:49:28 -0500

    made the navbar work and created some placeholder templates

--- SportsProject/sportApp/urls.py
+++ SportsProject/sportApp/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-
+ 
+ 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('', views.News, name = 'News'),
+    path('', views.home, name='home'),
+    path('news/', views.news, name='news'),
+    path('watchparty/' , views.watchparty , name = 'watchparty' ),
+    path('spaces/' , views.spaces , name = 'spaces' ),
+    path('clips/' , views.clips , name = 'clips' ),
+    path('edit_profile/' , views.edit_profile , name = 'edit_profile' ),
+    path('add_friends/' , views.add_friends , name = 'add_friends' ),
+    path('direct_messages/' , views.direct_messages , name = 'direct_messages' ),
+    path('premium/' , views.premium , name = 'premium' ),
 ]
-
+ 
  
--- SportsProject/sportApp/views.py
+++ SportsProject/sportApp/views.py
@@ -3,5 +3,29 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, 'home.html', {})
 
-def News(request):
-    return render(request, 'News.html',{})
+def watchparty(request):
+    return render(request, 'watchparty.html', {})
+
+def spaces(request):
+    return render(request, 'spaces.html', {})
+
+def clips(request):
+    return render(request, 'clips.html', {})
+
+def news(request):
+    return render(request, 'news.html',{})
+
+def edit_profile(request):
+    return render(request, 'edit_profile.html',{})
+
+def add_friends(request):
+    return render(request, 'add_friends.html',{})
+
+
+def direct_messages(request):
+    return render(request, 'direct_messages.html', {})
+
+def premium(request):
+    return render(request, 'premium.html',{})
+
+
